603464b1645bc4d6daf1f99de7e59fc3c8497b20
bormanico <97111633+bormanico@users.noreply.github.com>
2022-01-04 15:30:22 +0000

    teste

---
+++ Teste.java
@@ -0,0 +1,20 @@
+public class Teste
+
+public static void main(String[] args)
+{
+	int inteiro = 20;
+}
+
+public static int soma(int inteiro)
+{  
+	int i = 0;
+	
+	int soma = 0;
+	
+	while (i <= inteiro){
+		soma += i;
+		i++;
+	}
+
+	return soma;
+}
